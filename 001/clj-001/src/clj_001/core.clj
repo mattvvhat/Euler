1be99e845186a93263d01f236db577e53a53d841
Matt Derasoirs <matt@planwork.us>
2014-12-15 00:34:49 -0500

    :cake: Redo 001 in clojure kinda

--- 001/clj-001/src/clj_001/core.clj
+++
@@ -1,5 +0,0 @@
-(ns clj-001.core)
-
-(defn -main [& args]
-  (println 'x_x)
-  )
---
+++ 001/clojure/src/clj_001/core.clj
@@ -0,0 +1,15 @@
+; This solution is pretty literal... there are probably better ways
+(ns clj-001.core)
+
+(defn answer []
+  (def limit 1000)
+  (def sum3 (reduce + (range 0 limit 3)))
+  (def sum5 (reduce + (range 0 limit 5)))
+  (def sum15 (reduce + (range 0 limit 15)))
+  (- (+ sum3 sum5) sum15)
+  )
+
+
+(defn -main [& args]
+  (println (answer))
+  )
