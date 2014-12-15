f68fc201114a2d253d9e0511b060524a283f110f
Matt Derasoirs <matt@planwork.us>
2014-12-15 17:37:43 -0500

    :cyclone: 004

---
+++ 004/clojure/src/euler_004/core.clj
@@ -0,0 +1,34 @@
+(ns euler-004.core)
+
+; convert int to string
+(defn int-to-string [n]
+  (format "%d" n))
+
+(defn is-palindrome? [string]
+  (def front (first string))
+  (def back (last string))
+
+  (if (<= (count string) 1)
+    ; Return True
+    true
+
+    (if (= front back)
+      (is-palindrome? (subs string 1 (- (count string) 1)))
+      false)))
+
+
+
+(defn -main [& args]
+  (println "Starting...")
+
+  ; Print
+  (println 
+    ; the maximum
+    (apply max
+           ; filtered 
+           (filter
+             ; by is-paindrome(int-to-string(.))
+             (comp is-palindrome? int-to-string)
+             ; of the list a*b a, b in 100..1000
+             (for [a (range 100 1000) b (range 100 1000)] (* a b)))))
+  )
