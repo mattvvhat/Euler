f0b9ed8bddd088d31e3c03651df75b5529053eaa
mattvvhat <not.mattowen@gmail.com>
2015-03-17 17:54:10 -0400

    :eyes:

---
+++ 008/clj/euler-008/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to euler-008
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ 008/clj/euler-008/project.clj
@@ -0,0 +1,7 @@
+(defproject euler-008 "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :main euler-008.core
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]])
