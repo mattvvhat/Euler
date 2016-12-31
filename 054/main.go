6895840972e0ef814fb6ce1c9d733086f47fabf8
Matt Owen <not.mattowen@gmail.com>
2016-12-31 15:11:00 -0500

    :shaved_ice: straight + flush

--- 054/main.go
+++ 054/main.go
@@ -2,15 +2,74 @@ package main
 
 import (
 	"fmt"
+	"sort"
+	"strings"
 )
 
 type Card struct {
-	Value int
-	Suit  rune
+	Rank int
+	Suit rune
+}
+
+type CardList []Card
+
+type Hand struct {
+	Cards CardList
+}
+
+func (h CardList) Len() int {
+	return len(h)
+}
+
+func (h CardList) Less(i, j int) bool {
+	return h[i].String() < h[j].String()
+}
+
+func (h CardList) Swap(i, j int) {
+	h[i], h[j] = h[j], h[i]
+}
+
+func MakeHand(a, b, c, d, e Card) Hand {
+	hand := Hand{[]Card{a, b, c, d, e}}
+	sort.Sort(hand.Cards)
+	return hand
+}
+
+func (h Hand) String() string {
+	values := make([]string, 5, 5)
+	for i, v := range h.Cards {
+		values[i] = v.String()
+	}
+	return strings.Join(values, " ")
 }
 
 func (c Card) String() string {
-	return fmt.Sprintf("%d", c.Value) + string(c.Suit)
+	return fmt.Sprintf("%d", c.Rank) + string(c.Suit)
+}
+
+func (h *Hand) IsStraight() bool {
+	prev := h.Cards[0].Rank - 1
+
+	for _, v := range h.Cards {
+		if prev+1 != v.Rank {
+			return false
+		}
+		prev = v.Rank
+	}
+
+	return true
+}
+
+func (h *Hand) IsFlush() bool {
+	suit := h.Cards[0].Suit
+
+	for _, v := range h.Cards {
+		if suit != v.Suit {
+			return false
+		}
+	}
+
+	return true
 }
 
 func main() {
--- 054/main_test.go
+++ 054/main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"testing"
 )
 
@@ -18,4 +19,53 @@ func TestCardAsString(t *testing.T) {
 			t.Fail()
 		}
 	}
+}
+
+func TestHandString(t *testing.T) {
+	v := MakeHand(
+		Card{1, 'A'},
+		Card{1, 'A'},
+		Card{1, 'A'},
+		Card{1, 'A'},
+		Card{1, 'A'},
+	)
+	if v.String() != "1A 1A 1A 1A 1A" {
+		t.Fail()
+	}
+
+	if !v.IsFlush() {
+		t.Fail()
+	}
+
+	b := MakeHand(
+		Card{1, 'H'},
+		Card{2, 'H'},
+		Card{3, 'H'},
+		Card{4, 'H'},
+		Card{5, 'S'},
+	)
+
+	if b.IsFlush() {
+		t.Fail()
+	}
+
+	c := MakeHand(
+		Card{2, 'H'},
+		Card{3, 'H'},
+		Card{4, 'H'},
+		Card{5, 'S'},
+		Card{1, 'H'},
+	)
+
+	if c.String() != "1H 2H 3H 4H 5S" {
+		t.Fail()
+	}
+
+	if !c.IsStraight() {
+		t.Fail()
+	}
+}
+
+func TestNothing(t *testing.T) {
+	fmt.Println("k")
 }
