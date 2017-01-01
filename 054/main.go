db496b4f09d8a584f30106c7e7aa243f5a378293
Matt Owen <not.mattowen@gmail.com>
2017-01-01 12:47:57 -0500

    :wolf:

--- 054/main.go
+++ 054/main.go
@@ -23,7 +23,11 @@ func (h CardList) Len() int {
 }
 
 func (h CardList) Less(i, j int) bool {
-	return h[i].String() < h[j].String()
+	if h[i].Rank == h[j].Rank {
+		return h[i].Suit < h[j].Suit
+	} else {
+		return h[i].Rank < h[j].Rank
+	}
 }
 
 func (h CardList) Swap(i, j int) {
@@ -34,10 +38,42 @@ func LoadCard(v string) Card {
 	//if len(v) != 2 {
 	//	return Card{0, 'G'}
 	//}
-	rank, _ := strconv.Atoi(v[0 : len(v)-1])
+
+	var val int
+	rank := v[0 : len(v)-1]
+
+	switch rank {
+	case "T":
+		val = 10
+	case "J":
+		val = 11
+	case "Q":
+		val = 12
+	case "K":
+		val = 13
+	case "A":
+		val = 14
+	default:
+		val, _ = strconv.Atoi(rank)
+	}
 	suit := rune(v[len(v)-1])
 
-	return Card{rank, suit}
+	return Card{val, suit}
+}
+
+func LoadHand(v string) Hand {
+	cards := strings.Split(v, " ")
+	cardList := make([]Card, 5, 5)
+	var hand Hand
+
+	for i, c := range cards {
+		cardList[i] = LoadCard(c)
+	}
+
+	hand = Hand{cardList}
+	sort.Sort(hand.Cards)
+
+	return hand
 }
 
 func MakeHand(a, b, c, d, e Card) Hand {
@@ -55,7 +91,24 @@ func (h Hand) String() string {
 }
 
 func (c Card) String() string {
-	return fmt.Sprintf("%d", c.Rank) + string(c.Suit)
+	var rank string
+
+	switch c.Rank {
+	case 10:
+		rank = "T"
+	case 11:
+		rank = "J"
+	case 12:
+		rank = "Q"
+	case 13:
+		rank = "K"
+	case 14:
+		rank = "A"
+	default:
+		rank = fmt.Sprintf("%d", c.Rank)
+	}
+
+	return rank + string(c.Suit)
 }
 
 func (h *Hand) CardSets() map[int]int {
@@ -88,6 +141,59 @@ func (h *Hand) IsFlush() bool {
 	return true
 }
 
+func (h Hand) CardSet() map[int]int {
+	cardSet := make(map[int]int)
+
+	for _, c := range h.Cards {
+		cardSet[c.Rank] += 1
+	}
+
+	return cardSet
+}
+
+func (h Hand) CardDist() string {
+	values := make([]string, 0, 5)
+
+	for _, c := range h.CardSet() {
+		values = append(values, fmt.Sprintf("%d", c))
+	}
+
+	sort.Strings(values)
+
+	return strings.Join(values, " ")
+}
+
+func (h Hand) Score() int {
+	// cardSet := h.CardSet()
+
+	switch h.CardDist() {
+	case "1 1 1 2":
+		return 1
+	case "1 2 2":
+		return 2
+	case "1 1 3":
+		return 3
+	case "2 3":
+		return 6
+	case "1 4":
+		return 7
+	}
+
+	if h.IsStraight() {
+		if h.IsFlush() {
+			return 8
+		} else {
+			return 4
+		}
+	}
+
+	if h.IsFlush() {
+		return 5
+	}
+
+	return 0
+}
+
 func ToRank(val int) rune {
 	s := map[int]rune{
 		10: 'J',
@@ -102,6 +208,28 @@ func ToRank(val int) rune {
 		return r
 	} else {
 		return rune(fmt.Sprintf("%d", val)[0])
+	}
+}
+
+func WhoWins(lhs, rhs string) int {
+	player1 := LoadHand(lhs)
+	player2 := LoadHand(rhs)
+
+	if player1.Score() == player2.Score() {
+		for i, _ := range player1.Cards {
+			if player1.Cards[5-i-1].Rank < player1.Cards[5-i-1].Rank {
+				return +1
+			} else if player1.Cards[5-i-1].Rank > player1.Cards[5-i-1].Rank {
+				return -1
+			}
+		}
+		return 0
+	}
+
+	if player1.Score() < player2.Score() {
+		return 1
+	} else {
+		return -1
 	}
 }
 
--- 054/main_test.go
+++ 054/main_test.go
@@ -9,7 +9,7 @@ func TestCardAsString(t *testing.T) {
 
 	expectations := make(map[Card]string)
 
-	expectations[Card{14, 'H'}] = "14H"
+	expectations[Card{14, 'H'}] = "AH"
 	expectations[Card{9, 'D'}] = "9D"
 	expectations[Card{1, 'S'}] = "1S"
 	expectations[Card{8, 'C'}] = "8C"
@@ -70,6 +70,11 @@ func TestLoad(t *testing.T) {
 	expectations := map[string]Card{
 		"2S":  Card{2, 'S'},
 		"12H": Card{12, 'H'},
+		"JS":  Card{11, 'S'},
+		"TS":  Card{10, 'S'},
+		"QD":  Card{12, 'D'},
+		"KC":  Card{13, 'C'},
+		"AS":  Card{14, 'S'},
 	}
 
 	for expected, result := range expectations {
@@ -80,14 +85,45 @@ func TestLoad(t *testing.T) {
 	}
 }
 
-func TestPairs(t *testing.T) {
-	//expectations := map[string](map[int]int){
-	//	"1H 2H 3H 4H 4S": map[int]int{1: 1, 2: 2, 3: 3, 4: 2},
-	//}
+func TestLoadHand(t *testing.T) {
+	h := LoadHand("5H 5C 6S 7S KD")
+	if h.String() != "5C 5H 6S 7S KD" {
+		fmt.Println(h.String())
+		t.Fail()
+	}
 
-	//for expected, results := range expectations {
-	//	fmt.Println(expected, results)
-	//}
+	a := LoadHand("AH 5S 6D TC JS")
+	if a.String() != "5S 6D TC JS AH" {
+		fmt.Println(a.String())
+		t.Fail()
+	}
+}
+
+func areMapsEqual(lhs, rhs map[int]int) bool {
+	if len(lhs) != len(rhs) {
+		return false
+	}
+
+	for k, _ := range lhs {
+		if lhs[k] != rhs[k] {
+			return false
+		}
+	}
+	return true
+}
+
+func TestPairs(t *testing.T) {
+	expectations := map[string](map[int]int){
+		"AH 2H 3H 4H 4S": map[int]int{14: 1, 2: 1, 3: 1, 4: 2},
+		"AH AS AD AC 4S": map[int]int{14: 4, 4: 1},
+	}
+
+	for expected, results := range expectations {
+		if !areMapsEqual(LoadHand(expected).CardSet(), results) {
+			t.Fail()
+		}
+		// fmt.Println(LoadHand(expected).CardSet(), results)
+	}
 
 }
 
@@ -103,6 +139,57 @@ func TestToRank(t *testing.T) {
 		if ToRank(expected) != results {
 			t.Fail()
 		}
+	}
+}
+
+func TestScore(t *testing.T) {
+	if LoadHand("AC 6H 7S 9S 9D").CardDist() != "1 1 1 2" {
+		t.Fail()
+	}
+
+	if LoadHand("AC 6H 7S 8S 9D").CardDist() != "1 1 1 1 1" {
+		t.Fail()
+	}
+
+	if LoadHand("AC 6H 7S 8S 9D").Score() != 0 {
+		t.Fail()
+	}
+	if LoadHand("5S 5D AS TS JS").Score() != 1 {
+		t.Fail()
+	}
+	if LoadHand("5S 5D AS AC JS").Score() != 2 {
+		t.Fail()
+	}
+	if LoadHand("5S 5D 5S AC JS").Score() != 3 {
+		t.Fail()
+	}
+	if LoadHand("5S 6D 7S 8C 9S").Score() != 4 {
+		t.Fail()
+	}
+	if LoadHand("5S 6S 7S 8S TS").Score() != 5 {
+		t.Fail()
+	}
+	if LoadHand("5S 5D 5H AC AS").Score() != 6 {
+		t.Fail()
+	}
+	if LoadHand("5S 5D 5H 5C AS").Score() != 7 {
+		t.Fail()
+	}
+	if LoadHand("5S 6S 7S 8S 9S").Score() != 8 {
+		t.Fail()
+	}
+
+	// LoadHand("5C 6H 7S 8S 9D").Score
+}
+
+func TestWhoWins(t *testing.T) {
+	switch {
+	case WhoWins("5S 6S 7S 8S 9S", "5D 6D 7D 8D 2D") != -1:
+		t.Fail()
+	case WhoWins("5S 6S 7S 8S TS", "5D 6D 7D 8D 9D") != +1:
+		t.Fail()
+	case WhoWins("5S 6S 7S 8S TS", "5D 6D 7D 8D TD") != 0:
+		t.Fail()
 	}
 }
 
