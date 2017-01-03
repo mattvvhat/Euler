6087969342d66ae1b044f6289945bec867e14d6d
Matt Owen <not.mattowen@gmail.com>
2017-01-02 21:57:41 -0500

    :shaved_ice: Getting the copy command right

--- 060/combinatorics.go
+++ 060/combinatorics.go
@@ -4,6 +4,20 @@ import (
 	"fmt"
 )
 
+func extend(lhs, rhs []int) []int {
+	result := make([]int, len(lhs)+len(rhs), len(lhs)+len(rhs))
+
+	for i, _ := range lhs {
+		result[i] = lhs[i]
+	}
+
+	for i, _ := range rhs {
+		result[i+len(lhs)] = rhs[i]
+	}
+
+	return result
+}
+
 func factorial(n int) int {
 	result := 1
 	for i := 1; i < n; i += 1 {
@@ -12,16 +26,52 @@ func factorial(n int) int {
 	return result
 }
 
-func combinations() {
+func C(lis []int, k int) [][]int {
+	var result [][]int
+
+	switch {
+	case k == 0:
+		return make([][]int, 0, 0)
+	case k == 1:
+		result = make([][]int, len(lis), len(lis))
+
+		for i, v := range lis {
+			u := []int{v}
+			result[i] = make([]int, 1, 1)
+			copy(result[i], u)
+		}
+
+		return result
+	}
+	return nil
 }
 
-/**
- * Return a channel yielding array of indices
- */
+// Recursive
+func combinations(n int, k int, c *chan []int) {
+	if k == 0 {
+		return
+	}
+
+	if n == 0 {
+		return
+	}
+
+	for i := 0; i < n; i++ {
+		x := extend([]int{}, []int{1, 2, 3})
+		*c <- x
+	}
+}
+
+// Return a channel yielding array of indices
 func Combinations(n, k int) chan []int {
-	results := make(chan []int, 10)
-	results <- []int{1, 2, 3}
-	close(results)
+	length := factorial(n) / factorial(n-k) / factorial(k)
+	results := make(chan []int, length)
+
+	go func() {
+		combinations(n, k, &results)
+		close(results)
+	}()
+
 	return results
 }
 
--- 060/combinatorics_test.go
+++ 060/combinatorics_test.go
@@ -5,10 +5,70 @@ import (
 	"testing"
 )
 
+func TestExtend(t *testing.T) {
+	a := extend([]int{}, []int{})
+
+	if len(a) != 0 {
+		t.Fail()
+	}
+
+	b := extend([]int{1, 2, 3}, []int{4, 5})
+
+	if len(b) != 5 {
+		t.Fail()
+	}
+
+	if b[3] != 4 {
+		t.Fail()
+	}
+
+	if b[4] != 5 {
+		t.Fail()
+	}
+}
+
 func TestX(t *testing.T) {
-	c := Combinations(5, 3)
-	for val := range c {
-		fmt.Println(val)
+}
+
+func IsEqualIntArray(lhs, rhs []int) bool {
+	if len(lhs) != len(rhs) {
+		return false
+	}
+
+	for i, _ := range lhs {
+		if lhs[i] != rhs[i] {
+			return false
+		}
+	}
+
+	return true
+}
+
+func IsEqualIntArrayArray(lhs, rhs [][]int) bool {
+	if len(lhs) != len(rhs) {
+		return false
+	}
+
+	for i, _ := range lhs {
+		if !IsEqualIntArray(lhs[i], rhs[i]) {
+			return false
+		}
+	}
+
+	return true
+}
+
+func TestC(t *testing.T) {
+	vals := []int{1, 2, 3}
+
+	if !IsEqualIntArrayArray(C(vals, 0), [][]int{}) {
+		t.Fail()
+	}
+
+	x := [][]int{[]int{1}, []int{2}, []int{3}}
+
+	if !IsEqualIntArrayArray(C(vals, 1), x) {
+		t.Fail()
 	}
 }
 
