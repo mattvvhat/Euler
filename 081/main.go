633c4406e05bfa85b54b2b4255d9cefb8b2e9e25
Matt Owen <not.mattowen@gmail.com>
2017-01-22 19:55:37 -0500

    :wolf:

--- 081/main.go
+++ 081/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	g "github.com/whatever/math/graphs"
 )
 
 type Matrix struct {
@@ -30,20 +31,62 @@ func (m *Matrix) Get(i, j int) int {
 
 func ShortestDistances(m *Matrix) [][]int {
 	distances := make([][]int, m.Width*m.Height)
+
 	return distances
 }
 
 func main() {
+
 	m := NewMatrix(5, 5)
+
 	m.Apply(
 		131, 673, 234, 103, 18,
 		201, 96, 342, 965, 150,
 		630, 803, 746, 422, 111,
 		537, 699, 497, 121, 956,
-		807, 732, 524, 37, 331,
+		805, 732, 524, 37, 331,
 	)
 
-	fmt.Println("...", m)
-	fmt.Println(m.Get(2, 0))
-	fmt.Println(m.Get(4, 4))
+	graph := g.NewWeightedUGraph()
+
+	// Add Vertices
+	graph.AddVertex("start")
+
+	for i := 0; i < m.Height; i++ {
+		for j := 0; j < m.Width; j++ {
+			graph.AddVertex(fmt.Sprintf("%d,%d", i, j))
+		}
+	}
+
+	// Add Edges
+	graph.AddEdge("start", "0,0", float64(m.Get(0, 0)))
+
+	for i := 0; i < m.Height; i++ {
+		for j := 0; j < m.Width; j++ {
+
+			current := fmt.Sprintf("%d,%d", i, j)
+			weight := float64(m.Get(i, j))
+
+			if i > 0 {
+				above := fmt.Sprintf("%d,%d", i-1, j)
+				graph.AddEdge(above, current, weight)
+			}
+
+			if j > 0 {
+				left := fmt.Sprintf("%d,%d", i, j-1)
+				graph.AddEdge(left, current, weight)
+			}
+
+			fmt.Println(fmt.Sprintf("%d,%d = %d", i, j, m.Get(i, j)))
+		}
+	}
+
+	distances := graph.ShortestDistances("start")
+
+	// fmt.Println(distances)
+	// fmt.Println(distances["4,4"])
+	// fmt.Println(distances["1,1"])
+	fmt.Println(distances["4,4"])
+
+	fmt.Println("...")
 }
